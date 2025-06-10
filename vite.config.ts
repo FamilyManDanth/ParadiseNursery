6245f3dac1b8506a62aa0ebe7d486b25b840dc0b
FamilyManDanth <daniel.thomas0890@gmail.com>
2025-06-09 21:11:41 -0500

    final deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/paradise-nursery/",
+  base: "/ParadiseNursery/", // Case-sensitive, matches your repo name
   build: {
     assetsDir: "assets",
     rollupOptions: {
